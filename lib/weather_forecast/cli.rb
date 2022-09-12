0f284deee5bb6a48c6353c88633f0ccb73427075
Chuck Smith <chuck.smith@opscompass.com>
2022-09-12 14:48:18 -0400

    Clean up

--- lib/weather_forecast/cli.rb
+++ lib/weather_forecast/cli.rb
@@ -4,9 +4,13 @@ module WeatherForecast
   # Set up customer CLI
   class CLI
     def call
-      puts 'Running Weather Forecast...'
+      puts "Running Weather Forecast...#{ENV['WEATHER_API_KEY']}"
+      menu
     end
 
-    def menu; end
+    def menu
+      input_city = 'Leland, NC'
+      puts "Menu - #{input_city}"
+    end
   end
 end
